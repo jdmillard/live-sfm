3edf5a876c53bffba256f5170ea7f5a6f41b7676
Jeffrey Millard <jeffrey.d.millard@gmail.com>
2017-04-03 16:20:07 -0600

    notes

--- src/circles.cpp
+++ src/circles.cpp
@@ -2,6 +2,16 @@
 
 using namespace cv;
 
+/*
+The class circleFinder is the visual front end that provides the pixel locations
+of spheres in the given frame. This can be replaced by any other class so long
+as it generates information for the structureFromMotion object in a compatible
+format.
+*/
+
+// perhaps this class is passed to the sfm object upon initialization
+// I would need to look into the best way to do this, perhaps inheritance
+
 circleFinder::circleFinder(int in_var)
 {
   my_pri_val = in_var;
