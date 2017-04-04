ed498416575f8b88451fe0d512ace01e97a2e9f2
Jeffrey Millard <jeffrey.d.millard@gmail.com>
2017-04-03 22:06:34 -0600

    read video frames

--- src/main.cpp
+++ src/main.cpp
@@ -18,17 +18,58 @@ int main(int argc, char **argv)
   // leaning towards hough circles
 
 
+
+  // create the video object
+  bool from_recorded = true;
+  VideoCapture video;
+  if (from_recorded)
+  {
+    // use frames from a pre-recorded video
+    std::string filename = "../images/vid.mp4";
+    std::cout << filename << std::endl;
+    video.open(filename);
+  }
+  else
+  {
+    // use frames from a local webcam
+    video.open(0);
+  }
+
+  namedWindow("The Frame", CV_WINDOW_AUTOSIZE);
+  moveWindow("The Frame", 50, 50);
+
+
   SphereDetector nextTest(13);
-  SphereDetector nextTest2(14);
+
   std::cout << nextTest.my_pub_val << std::endl;
   std::cout << nextTest.my_pub_valy << std::endl;
   std::cout << nextTest.getVal() << std::endl;
   std::cout << nextTest.getValy() << std::endl;
-  std::cout << "----" << std::endl;
-  std::cout << nextTest2.my_pub_val << std::endl;
-  std::cout << nextTest2.my_pub_valy << std::endl;
-  std::cout << nextTest2.getVal() << std::endl;
-  std::cout << nextTest2.getValy() << std::endl;
+
+
+  Mat frame;
+
+  while (true)
+  {
+    video >> frame;
+    std::cout << frame.size() << std::endl;
+
+    imshow("The Frame", frame);
+
+    int key = waitKey();
+    if (key == 110) {
+      // the 'n' (next) key was pressed
+    } else if (key == 27) {
+      // the 'esc' key was pressed, end application
+      std::cout << "terminating" << std::endl;
+      return -1;
+    }
+
+
+
+
+  }
+
 
   std::cout << "terminating" << std::endl;
 }
