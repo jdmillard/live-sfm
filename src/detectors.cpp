2695a7eb566588c9ac184c45f7e750da703cf52e
Jeffrey Millard <jeffrey.d.millard@gmail.com>
2017-04-04 22:43:17 -0600

    improved edges

--- src/detectors.cpp
+++ src/detectors.cpp
@@ -27,37 +27,47 @@ int SphereDetector::getVal()
 void SphereDetector::newFrame(Mat frame_in)
 {
 
-  //not sure if blur is needed, if it goes before or after, or which type
-  //before seems common
-  // guassian blur params
-  GaussianBlur(frame_in, frame, Size(9,9), 2,2);
+  // perform image processing required to locate spheres in the current frame
 
-  // a new frame is passed in and circles need to be detected
-  cvtColor(frame, frame, CV_BGR2GRAY);
+  // convert to grayscale
+  cvtColor(frame_in, frame, CV_BGR2GRAY);
 
-	// morphology params
-	int morph_size = 10;   // kernel size
-  Mat element = getStructuringElement( MORPH_RECT, Size( 2*morph_size + 1, 2*morph_size+1 ), Point( morph_size, morph_size ) );
-  morphologyEx( frame, frame, MORPH_OPEN, element );
+  // blur the image to soften background
+  int blur_size = 15;     // size of the blur kernel
+  int sigma = 5;          // variance
+  GaussianBlur(frame, frame, Size(blur_size, blur_size), sigma, sigma);
 
-  // canny
-  Canny(frame, frame, 50, 100);
+	// perform open and close to clean up the foreground
+	int morph_size = 4;     // high number makes circles boxy
+  Mat element = getStructuringElement(MORPH_RECT, Size(2*morph_size + 1, 2*morph_size+1), Point(morph_size, morph_size));
+  morphologyEx(frame, frame, MORPH_OPEN, element );
 
-  // perform HoughCircles on the grayscale image
-  /*
+  // detect the edges
+  double thresh1 = 80;    // hysteresis threshold 1, low as possible
+  double thresh2 = 80;    // hysteresis threshold 2, low as possible
+  Canny(frame, frame, thresh1, thresh2);
+
+  // at this point, clean edges are expected around the circles
+
+  // blur the resulting white lines in order to find circles easier
+  int blur_size2 = 35;    // size of the blur kernel
+  int sigma2 = 5;         // variance
+  GaussianBlur(frame, frame, Size(blur_size2, blur_size2), sigma2, sigma2);
+
+  // perform HoughCircles on the edges
   std::vector<Vec3f> circles;
-  double dp = 1;
-  double minDist = 100;
-  double param1 = 80;
-  double param2 = 40;
+  double dp = 2;
+  double minDist = 30;
+  double param1 = 50;
+  double param2 = 140;
   int minRadius = 0;
-  int maxRadius = 100;
+  int maxRadius = 110;
   HoughCircles(frame, circles, CV_HOUGH_GRADIENT, dp, minDist, param1, param2, minRadius, maxRadius);
-  */
 
 
+  cvtColor(frame, frame, CV_GRAY2BGR);
 
-  //drawCircles(frame_in, circles);
+  drawCircles(frame, circles);
   //frame = frame_in;
 
   //frame = frame_in;
